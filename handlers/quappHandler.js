f73ec8d95ad56a6eb3e57cd446bdbaa89dbc7d9b
kagaba-etienne <kagabaetienne04@gmail.com>
2023-08-09 22:43:14 +0200

    hotfix

--- handlers/quappHandler.js
+++ handlers/quappHandler.js
@@ -5,7 +5,6 @@ const sendApp = (req, res) => {
     const host = req.hostname;
     const filePath = req.protocol + "://" + host + '/' + req?.file?.path;
     req.body = {...req.body, resume: [filePath]};
-    console.log(req)
     const uQuapp = new Quapp({...req.body, readStatus: false});
     uQuapp.save()
         .then(_ => {
